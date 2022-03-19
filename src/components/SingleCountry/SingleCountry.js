39848b9195b7e5f1d51db346f32184e46c34e704
foadtkf <fakhrulif@gmail.com>
2022-03-19 13:32:14 +0600

    fixed

--- src/components/SingleCountry/SingleCountry.js
+++ src/components/SingleCountry/SingleCountry.js
@@ -5,9 +5,10 @@ const SingleCountry = (props) => {
         <div className='country'>
         <div className='head'>
             <h4>Name: {props.country.name.common}</h4>
-            <img src={props.country.coatOfArms.png}className='coatOA'></img>
+            <img src={props.country.coatOfArms.png} className='coatOA'  alt="" srcset="" ></img>
         </div> 
-            <img src={props.country.flags.png}></img>
+            
+            <img src={props.country.flags.png}  alt="" srcset="" ></img>
             <p>Population: {props.country.population}</p>
             <p><small>Area: {props.country.area}</small></p>
         </div>
